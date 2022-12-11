a82bb4ecdf9f0f55079950675ad7458baa02b676
Mahmoud Aly <31845620+m37moud@users.noreply.github.com>
2022-12-11 13:04:52 +0200

    Update build.gradle.kts
    githup 2022/12/11

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,9 +1,35 @@
+//https://medium.com/@theendik00/sqldelight-for-postgresql-on-kotlin-jvm-b95d14d96134
 
 plugins {
     kotlin("jvm")
     kotlin("kapt")
-    id("com.squareup.sqldelight") version "1.5.4"
+    id("com.squareup.sqldelight") //version "1.5.4"
 }
+
+
+// sqldelight {
+//     database(name = "HockeyDatabase") { // This will be the name of the generated database class.
+//         packageName = "com.HrAppV"
+//         dialect = "postgresql"
+//         deriveSchemaFromMigrations = true
+//     }
+// }
+
+// pluginManagement {
+//     repositories {
+//         google()
+//         mavenCentral()
+//         gradlePluginPortal() // Not sure if you need this
+//     }
+
+//     resolutionStrategy {
+//         eachPlugin {
+//             if (requested.id.id == "com.squareup.sqldelight") {
+//                 useModule("com.squareup.sqldelight:gradle-plugin:1.5.3")
+//             }
+//         }
+//     }
+// }
 
 group = "com.HrAppV"
 version = "1.0.0"
@@ -34,12 +60,25 @@ dependencies {
     implementation("com.squareup.sqldelight:jdbc-driver:$sqlDelight")
 
 
+//     // PostgreSQL JDBC driver
+//     implementation("org.postgresql:postgresql:42.5.0")
+    
+//     // Hikari requires SLF4J
+//     implementation("org.slf4j:slf4j-reload4j:2.0.0")
+    
+//     // Hikari connection manager
+//     implementation("com.zaxxer:HikariCP:5.0.1")
+    
+//     // Converts javax.sql.DataSource from your connection manager to com.squareup.sqldelight.db.JdbcDriver
+//     implementation("com.squareup.sqldelight:jdbc-driver:1.5.3")
+
+
 }
 
 
-sqldelight {
-    sqldelight.com.squareup.sqlite.migrations.Database { // This will be the name of the generated database class.
-        packageName = "com.HrAppV"
-        dialect = "mysql"
-    }
-}
+// sqldelight {
+//     sqldelight.com.squareup.sqlite.migrations.Database { // This will be the name of the generated database class.
+//         packageName = "com.HrAppV"
+//         dialect = "mysql"
+//     }
+// }
