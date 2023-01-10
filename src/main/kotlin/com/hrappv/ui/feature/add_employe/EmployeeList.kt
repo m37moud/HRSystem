34a468c6a8454b064d9de424d9ebe2045b1b0a28
work <m37moud00@gmail.com>
2023-01-10 15:49:26 +0200

    from work 2023/1/10 .

---
+++ src/main/kotlin/com/hrappv/ui/feature/add_employe/EmployeeList.kt
@@ -0,0 +1,138 @@
+
+//https://johncodeos.com/how-to-add-search-in-list-with-jetpack-compose/
+@Composable
+fun MainScreen() {
+    val textState = remember { mutableStateOf(TextFieldValue("")) }
+    Column {
+        SearchView(textState)
+        ItemList(state = textState)
+    }
+}
+
+@Composable
+fun SearchView(state: MutableState<TextFieldValue>) {
+    TextField(
+        value = state.value,
+        onValueChange = { value ->
+            state.value = value
+        },
+        modifier = Modifier.fillMaxWidth(),
+        textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
+        leadingIcon = {
+            Icon(
+                Icons.Default.Search,
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(15.dp)
+                    .size(24.dp)
+            )
+        },
+        trailingIcon = {
+            if (state.value != TextFieldValue("")) {
+                IconButton(
+                    onClick = {
+                        state.value =
+                            TextFieldValue("") // Remove text from TextField when you press the 'X' icon
+                    }
+                ) {
+                    Icon(
+                        Icons.Default.Close,
+                        contentDescription = "",
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .size(24.dp)
+                    )
+                }
+            }
+        },
+        singleLine = true,
+        shape = RectangleShape, // The TextFiled has rounded corners top left and right by default
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color.White,
+            cursorColor = Color.White,
+            leadingIconColor = Color.White,
+            trailingIconColor = Color.White,
+            backgroundColor = MaterialTheme.colors.primary,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        )
+    )
+}
+
+
+
+
+@Composable
+fun ItemList(state: MutableState<TextFieldValue>) {
+    val items by remember { mutableStateOf(listOf("Drink water", "Walk")) }
+
+    var filteredItems: List<String>
+    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+        val searchedText = state.value.text
+        filteredItems = if (searchedText.isEmpty()) {
+            items
+        } else {
+            val resultList = ArrayList<String>()
+            for (item in items) {
+                if (item.lowercase(Locale.getDefault())
+                        .contains(searchedText.lowercase(Locale.getDefault()))
+                ) {
+                    resultList.add(item)
+                }
+            }
+            resultList
+        }
+        items(filteredItems) { filteredItem ->
+            ItemListItem(
+                ItemText = filteredItem,
+                onItemClick = { /*Click event code needs to be implement*/
+                }
+            )
+        }
+
+    }
+}
+
+
+
+@Composable
+fun ItemListItem(ItemText: String, onItemClick: (String) -> Unit) {
+    Row(
+        modifier = Modifier
+            .clickable(onClick = { onItemClick(ItemText) })
+            .background(colorResource(id = R.color.purple_700))
+            .height(57.dp)
+            .fillMaxWidth()
+            .padding(PaddingValues(8.dp, 16.dp))
+    ) {
+        Text(text = ItemText, fontSize = 18.sp, color = Color.White)
+    }
+}
+
+
+
+@Composable
+fun EmployeeScreen() {
+    val employees: List<Employee> = getEmployees()
+    val filteredEmployees = state { employees }
+
+    Column {
+        TextField(
+            value = state { "" },
+            onValueChange = { filteredEmployees.value = employees.filter { it.name.contains(it) } },
+            label = { Text("Search Employee") }
+        )
+
+        RecyclerView(data = filteredEmployees.value) { employee ->
+            Card(modifier = Modifier.clickable(onClick = {
+                navigateTo(employee)
+            })) {
+                Column {
+                    Text(employee.name)
+                    Text(employee.position)
+                }
+            }
+        }
+    }
+}
