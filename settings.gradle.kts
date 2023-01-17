c76c79cd1827d5d79b9508fc38c902ad51836955
Mahmoud Aly <31845620+m37moud@users.noreply.github.com>
2023-01-17 14:25:18 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
       //  maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
         google()
         gradlePluginPortal()
+        mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 
